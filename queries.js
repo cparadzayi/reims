09fb6f948e20db692915ffecd9c75dca3d6a629a
cparadzayi <cparadzayi@gmail.com>
2017-02-21 11:06:18 +0200

    Revert "Expose the getAccountByName function"
    This reverts commit 9826c74520a385f0aa11e80d5fd9a25e6ce7348c.

--- queries.js
+++ queries.js
@@ -160,7 +160,6 @@ function getCadastralData(req, res, next){
 module.exports = {
   getAllclients: getAllclients,
   getSingleAccount: getSingleAccount,
-  getAccountByName:getAccountByName,
   createAccount: createAccount,
   updateAccount: updateAccount,
   removeAccount: removeAccount,
